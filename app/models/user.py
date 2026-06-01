21bc0d1f61ba298746bbe2858f52d0f7190c9b3d
luxis8 <lucacarta918@gmail.com>
2026-06-01 11:18:55 +0200

    Correzione modello User

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,39 @@
+from pydantic import StrictStr
 from sqlmodel import SQLModel, Field
 
 
-class User(SQLModel, table=True):
+class UserBase(SQLModel):
     """
-    Modello ORM che rappresenta un utente del sistema.
+    Attributi comuni dell'utente.
+    """
+
+    username: str
+    name: str
+    email: str
+
+
+class User(UserBase, table=True):
+    """
+    Modello ORM che rappresenta un utente del sistema nel database.
     """
 
     username: str = Field(primary_key=True)
-    name: str
-    email: str
+
+
+class UserCreate(SQLModel):
+    """
+    Modello usato per creare un utente tramite API.
+    Serve per validare correttamente i dati in input.
+    """
+
+    username: StrictStr
+    name: StrictStr
+    email: StrictStr
+
+
+class UserPublic(UserBase):
+    """
+    Modello usato per restituire un utente tramite API.
+    """
+
+    pass
